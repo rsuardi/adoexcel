34d9b07ce232a1ca5a80b82c3eebf340cb09bb72
Elisardo Felix <elisardofelix@gmail.com>
2016-11-30 23:32:24 -0400

    Remove the unnecesary methods

--- ADOExcel/FileReader.cs
+++ ADOExcel/FileReader.cs
@@ -13,9 +13,6 @@ namespace ADOExcel
     //Test Mod Elisardo
     class FileReader<T>
     {
-        private static List<T> list = new List<T>();
-
-        public FileReader() { }
 
         public static OleDbConnection OpenExcel(string file = null)
         {
@@ -62,68 +59,7 @@ namespace ADOExcel
             }
         }
 
-        public static List<T> ReadExcel(string file)
-        {
-            list.Clear();
-            var conn = OpenExcel(file);
-            var ds1 = SelectWorkingSheet(conn);
-
-            var F1 = ds1.Tables["Table"].Columns["F1"];
-            var F2 = ds1.Tables["Table"].Columns["F2"];
-            var F3 = ds1.Tables["Table"].Columns["F3"];
-            var F4 = ds1.Tables["Table"].Columns["F4"];
-
-            if (F1 == null || F2 == null || F3 == null || F4 == null)
-            {
-                throw new ArgumentException("There are columns missing in your document please verify");
-            }
-
-            string excelHeader = "<table cellspacing='0' rules='all' border='1'  style='border-collapse:collapse;'><tbody><tr><th scope='col'>Order #</th><th scope='col'>Part #</th><th scope='col'>IMEI</th><th scope='col'>Model</th></tbody></table>";
-
-            if (ds1.Tables["Table"].Rows[0][0].ToString() != "Order #" && ds1.Tables["Table"].Rows[0][1].ToString() != "Part #" && ds1.Tables["Table"].Rows[0][2].ToString() != "IMEI" && ds1.Tables["Table"].Rows[0][3].ToString() != "Model")
-                throw new ArgumentException(String.Format("Sorry, I could not find the specific format. Your document header must be {0} ", excelHeader));
-
-            var finalList = new List<T>();
-            for (int i = 0; i <= ds1.Tables["Table"].Rows.Count - 1; i++)
-            {
-                if (i == 0)
-                    continue;
-
-                if (String.IsNullOrEmpty(ds1.Tables["Table"].Rows[i]["F1"].ToString()) && String.IsNullOrEmpty(ds1.Tables["Table"].Rows[i]["F2"].ToString()) && String.IsNullOrEmpty(ds1.Tables["Table"].Rows[i]["F3"].ToString()) && String.IsNullOrEmpty(ds1.Tables["Table"].Rows[i]["F4"].ToString()))
-                    continue;
-                //throw new ArgumentException(String.Format("Sorry, I could not load all the data. I found one empty starting at row #{0}", i + 1));
-
-                if (String.IsNullOrEmpty(ds1.Tables["Table"].Rows[i]["F1"].ToString()) || String.IsNullOrEmpty(ds1.Tables["Table"].Rows[i]["F3"].ToString()))
-                    throw new ArgumentException(String.Format("Sorry, I could not load all the data. I found essensial values missing starting at row #{0}", i + 1));
-
-                if (ds1.Tables["Table"].Rows[i]["F3"].ToString().Length < 15)
-                    throw new ArgumentException(String.Format("Sorry, I could not load all the data. I found one IMEI with the wrong lenght at row #{0}", i + 1));
-
-
-                var lot = ds1.Tables["Table"].Rows[i]["F1"].ToString();
-
-                object q = new object();
-                //q.ORDERNUMBER = ds1.Tables["Table"].Rows[i]["F1"].ToString();
-                //q.SKU = ds1.Tables["Table"].Rows[i]["F2"].ToString();
-                //q.IMEI = ds1.Tables["Table"].Rows[i]["F3"].ToString();
-                //q.MODEL = ds1.Tables["Table"].Rows[i]["F4"].ToString();
-                //q.CREATIONDATE = DateTime.Now;
-                //q.LOTTYPE = lotType;
-                //q.USERID = 12;
-                //q.STATUSID = 1;
-
-                //list.Add();
-            }
-            //finalList = list.DistinctBy(x => new { x.ORDERNUMBER, x.IMEI }).ToList();
-
-            try
-            {
-                return finalList;
-            }
-            catch (Exception ex)
-            {
-                throw new ArgumentException("There was a problem while reading the file");
-            }
-        }
+        
+        
     }
 }
