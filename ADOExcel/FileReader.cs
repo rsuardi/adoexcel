0bc5d1bfe2a22ac7a7d08d1014e426a906f6a0b2
Elisardo Felix <elisardofelix@gmail.com>
2016-12-01 00:04:30 -0400

    New functionality of findList
    This functionality find the fields on the selected sheet using the modeling of a object properties and creating a generic list on the result with the same type.

--- ADOExcel/FileReader.cs
+++ ADOExcel/FileReader.cs
@@ -1,5 +1,4 @@
-﻿using MoreLinq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
@@ -10,8 +9,14 @@ using System.Threading.Tasks;
 
 namespace ADOExcel
 {   
-    //Test Mod Elisardo
-    class FileReader<T>
+
+    class where
+    {
+        public string name { get; set; }
+        public string columname { get; set; }
+    }
+
+    class FileReader
     {
 
         public static OleDbConnection OpenExcel(string file = null)
@@ -56,10 +61,115 @@ namespace ADOExcel
             catch (Exception ex)
             {
                 return null;
+            }   
+        }
+
+        public static List<T> findList<T>(DataTable data) where T : new ()
+        {
+            List<T> Temp = new List<T>();
+            T Tempo = new T();
+
+
+
+            PropertyInfo[] h = Tempo.GetType().GetProperties();
+            List<string> find = new List<string>();
+            
+
+            foreach (PropertyInfo property in h)
+            {
+                    find.Add(property.Name); 
             }
+
+            int des = -1;
+            List<where> found = new List<where>();
+            bool finded = false;
+
+            
+            for (int i = 0; i < data.Columns.Count; i++)
+            {   
+                
+                for (int x = 0; x < data.Rows.Count; x++)
+                {
+                    if (finded == false)
+                    {
+                        foreach (string on in find)
+                        {
+                            if (data.Rows[x][i].ToString() == on)
+                            {
+                                finded = true;
+                                des = x + 1;
+                                found = scanl(data.Rows[x], find);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                        break;
+                }
+                if (finded)
+                    break;
+            }
+            
+            for(int i = des; i < data.Rows.Count; i++)
+            {
+                Tempo = new T();
+                foreach (where s in found)
+                {
+                    if (s.columname != null)
+                    {
+
+                        PropertyInfo propertyInfo = typeof(T).GetProperty(s.name);
+                        propertyInfo.SetValue(Tempo, data.Rows[i][s.columname].ToString(), null);
+                    }
+                    else
+                    {
+                        PropertyInfo propertyInfo = typeof(T).GetProperty(s.name);
+                        propertyInfo.SetValue(Tempo, string.Empty);
+                    }
+                }
+                Temp.Add(Tempo);
+            }   
+
+            return Temp;
+        }
+
+        private static List<where> scanl(DataRow here, List<string> values)
+        {
+            List<where> temp = new List<where>();
+            where tempo = new where();
+            
+
+            foreach (string val in values)
+            {
+                bool founded = false;
+                foreach (DataColumn col in here.Table.Columns)
+                {   
+                    if (here[col].ToString() == val)
+                    {
+                        tempo = new where();
+                        tempo.columname = col.ColumnName.ToString();
+                        tempo.name = val;
+                        temp.Add(tempo);
+                        founded = true;
+                    }
+                }
+
+                if(!founded)
+                {
+                    tempo = new where();
+                    tempo.columname = null;
+                    tempo.name = val;
+                    temp.Add(tempo);
+                }
+
+            }
+            
+
+            return temp;
+        }
         
 
-        
-        
     }
+
+
 }
